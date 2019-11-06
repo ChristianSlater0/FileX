13372624ddc89a17d99089725e6a8dd7233e05c7
Christian Slater <Slaterchristian0@yahoo.com>
2019-11-05 20:00:08 -0600

    deleted garbage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,34 +14,33 @@ export class AppComponent {
 //test garbage
 
 form: FormGroup;
-  orders = [];
+directory = [];
 
   constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
-      orders: ['']
+      directory: ['']
     });
-
-    // mimic async orders
-    of(this.getOrders()).subscribe(orders => {
-      this.orders = orders;
-      this.form.controls.orders.patchValue(this.orders[0].id);
+    of(this.getDirectory()).subscribe(directory => {
+      this.directory = directory;
+      this.form.controls.orders.patchValue(this.directory[0].id);
     });
-
-    // synchronous orders
-    // this.orders = this.getOrders();
-    // this.form.controls.orders.patchValue(this.orders[0].id);
   }
 
-  getOrders() {
+  getDirectory() {
     return [
-      { id: 4096, name: 'NODE', value: 'fa fa-file' },
-      { id: 365, name: 'Notes', value: 'fa fa-file' },
+      { id: 4096, name: 'NODE', value: 'fa fa-folder' },
+      { id: 365, name: 'Notes', value: 'fa fa-folder' },
       { id: 2, name: 'File 3', value: 'fa fa-file' },
       { id: 2, name: 'File 4', value: 'fa fa-file' }
     ];
   }
  
+  doSomething() {
+    this.directory = null;
+  }
+ 
   submit() {
+    //this does nothing
     console.log(this.form.value);
   }
 
