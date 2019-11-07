f579946f74a314c2872dac19a7f896fa7f68c4f0
BenjaminSipe <benjaminmsipe@student.cofo.edu>
2019-11-07 10:57:58 -0600

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,38 +12,65 @@ export class AppComponent {
   title = 'my-app';
 
 
-//test garbage
+  //test garbage
 
-form: FormGroup;
-directory = [];
+  form: FormGroup;
+  directory = [];
 
   constructor(private formBuilder: FormBuilder, public http: HttpClient) {
     this.form = this.formBuilder.group({
       directory: ['']
     });
-    localStorage.setItem("path","/home")
+    localStorage.setItem("path", "/home")
     this.getDirectory()
   }
 
   getDirectory() {
-    this.http.get(`/api/ls?path=${localStorage.getItem("path")}`).subscribe((response : any[]) => {
+    this.http.get(`/api/ls?path=${localStorage.getItem("path")}`).subscribe((response: any[]) => {
       // console.log(response);
-      this.directory = response});
+      this.directory = response
+    });
   }
- 
-  doSomething(filder: any) {
-    console.log(filder)
-    localStorage.setItem("path", localStorage.getItem("path") + "/" + filder.name)
-    this.getDirectory();
-    // this.directory =
+
+  toParent() {
+    var str = localStorage.getItem("path");
+    if (str != "/home") {
+      localStorage.setItem("path", str.substring(0, str.lastIndexOf("/")));
+      this.getDirectory();
+    }
+
+  }
+
+  createFile(name: string) {
     
+    if (name) {
+      this.http.put(`/api/mkfile?path=${localStorage.getItem("path")}&name=${name}`, null).subscribe((obj)=>{});
+      this.getDirectory();
+    }
+  }
+  createFolder(name: string) {
+    
+    if (name) {
+      this.http.put(`/api/mkfile?path=${localStorage.getItem("path")}&name=${name}`, null).subscribe((obj)=>{});
+      this.getDirectory();
+    }
+  }
+
+  cd(filder: any) {
+    if (filder.isDirectory) {
+
+      localStorage.setItem("path", localStorage.getItem("path") + "/" + filder.name)
+      this.getDirectory();
+    }
+    // this.directory =
+
     // [
     //   { id: 4096, name: 'NOsadfDE', value: 'fa fa-folder' },
     //   { id: 365, name: 'Noasdftes', value: 'fa fa-folder' },
     //   { id: 2, name: 'Filasdfsadfe 3', value: 'fa fa-file' }
     // ];;
   }
- 
+
   submit() {
     //this does nothing
     console.log(this.form.value);
